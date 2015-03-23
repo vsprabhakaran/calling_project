bb98a07e42fbac10be6891f912bfcc333c78a520
vsprabhakaran <v.s.prabhakaran@gmail.com>
2015-03-23 11:28:42 +0530

    Prabhakaran: 1. New status added to the drop box and added sample cheque image.

--- mainpage.php
+++ mainpage.php
@@ -5,6 +5,11 @@
   <link rel="stylesheet" href="css/jquery-ui.min.css">
   <link rel="stylesheet" href="css/globalStyles.css">
   <link rel="stylesheet" href="css/pure-min.css">
+  <?php
+	session_start();
+	$_SESSION["pf_index"] = "6716547";
+	$_SESSION["emp_name"] = "Prabhakaran S"
+  ?>
   <script>
   $(function() {
     $( "#datepicker" ).datepicker();
@@ -40,9 +45,26 @@ input[type=button]{
 
 </head>
 <body>
+	<script type="text/javascript">
+	var pf;
+	$(document).ready(function () {
+		$('#formid').bind("keyup keypress", function (e) {
+			var code = e.keyCode || e.which;
+			if (code == 13) {
+				e.preventDefault();
+				return false;
+			}
+		});
+		$("#chequeDetailsPanel").hide();
+		$("#chequeButtonPanel").hide();
+		$("#chequeListPanel").hide();
+		$("#chequeImagePanel").hide();
+	});
+	</script>
+
    <div id="header">
        <div style="margin-left:40%;display:inline-block;">SBI CCPC,CHENNAI</div>
-       <div style="display:inline-block;float:right;"> Sample PF No (<a>Logout</a>)</div>
+       <div style="display:inline-block;float:right;"> <?php echo $_SESSION["pf_index"];?> (<a>Logout</a>)</div>
    </div>
    <div>
      <iframe src='' style="height:5em;width:100%;" ></iframe>
@@ -61,8 +83,13 @@ input[type=button]{
      <input type="button" name="filterButton" id="filterButton" value="submit" />
    </div>
 
+   <form name="chequeProcessingForm" id="chequeProcessingForm" method="post" action="">
+   	<!-- Storing the processing state-->
+	<input type="hidden" name="date_chosen" value="none"/>
+	<input type="hidden" name="circle_chosen" value="none"/>
+	
    <div id="chequeImagePanel">
-     <img id="chequeImage" src="sample.jpg" height=300 width=660 />
+     <img id="chequeImage" src="img/StateBankofIndia_IN.png" height=300 width=660 />
    </div>
    <div id="chequeListPanel">
      <table id="chequeList" border=1 style="border-collapse:collapse">
@@ -89,18 +116,24 @@ input[type=button]{
      <label for="phoneNumber">Phone Number:</label>
      <input type="text" id="phoneNumber" name="phoneNumber" style="width:10%"/>
      <label for="callStatus">Call Status:</label>
-     <select id="callStatus">
-       <option value="cc">Contacted Customer</option>
-       <option value=cbp>Contacted branch through phone</option>
-       <option value="cbm">Contacted branch through mail</option>
-       <option value="pb">Customer number busy</option>
-       <option value="pnr">Customer number not reachable</option>
+     <select id="callStatus" name="callStatus">
+       <option value="CC">Confirmed by Customer</option>
+       <option value="CBP">Confirmed by branch through phone</option>
+       <option value="CBM">Confirmed by branch through mail</option>
+	   <option value="DC">Denied by Customer</option>
+	   <option value="DBP">Denied by branch through phone</option>
+	   <option value="DBM">Denied by branch through mail</option>
+       <option value="PB">Customer number busy</option>
+       <option value="PNR">Customer number not reachable</option>
      </select>
+
+	<br/><br/>
+
    </div>
-   <br/><br/>
-   <div>
+   	<div id="chequeButtonPanel">
      <input type="button" name="prevButton" id="prevButton" value="Previous" />
      <input type="button" name="nextButton" id="nextButton" value="Next" />
-   </div>
+	</div>
+	</form>
 </body>
 </html>
